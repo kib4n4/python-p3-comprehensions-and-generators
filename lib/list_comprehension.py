5eba5e314e01d173b3e95ccc6ce074c35f31e8b0
Luther Isaboke <lusaboke@gmail.com>
2024-05-30 14:12:43 +0300

    list comprehension

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
+# This line specifies that this script should be executed using the Python 3 interpreter.
 
+# This function takes a list of integers as input and returns a new list containing only the even elements.
 def return_evens(num_list):
-    pass
+    # The list comprehension [x for x in num_list if x % 2 == 0] creates a new list
+    # where each element x is included if it is even (x % 2 == 0).
+    return [x for x in num_list if x % 2 == 0]
 
+# This function takes a list of sentence strings as input and returns a new list
+# where each sentence has an exclamation mark added to the end.
 def make_exclamation(sentence_list):
-    pass
+    # The list comprehension [sentence + "!" for sentence in sentence_list] creates a new list
+    # where each sentence string has an exclamation mark concatenated to the end.
+    return [sentence + "!" for sentence in sentence_list]
